feba813d795728b16a90bf48c3fafbc72cef8851
JIANZON <try19623@gmail.com>
2017-07-27 09:57:39 +0800

    Git look

---
+++ P001/src/Helloer01.java
@@ -0,0 +1,8 @@
+
+public class Helloer01 {
+	
+	public void sayHello(){
+		System.out.println("I'm a helloer.");
+	}
+	
+}
---
+++ P001/src/RunnerP1.java
@@ -0,0 +1,10 @@
+
+public class RunnerP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Helloer01 helloer01 = new Helloer01();
+		helloer01.sayHello();
+	}
+
+}
